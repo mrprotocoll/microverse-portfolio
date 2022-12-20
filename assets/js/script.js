19916dda65390c0b9220ccb7ece4ec350b37cc80
mrprotocoll <lekanvgbg@gmail.com>
2022-12-20 16:45:48 +0100

    Add modal pop up script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,71 @@
+// function to toggle navbar
 const toggleNavbar = () => {
   document.querySelector(".navbar").classList.toggle("show");
 };
 
+// modal pop up Template
+const modal = (work) => {
+  return `<div class="modal">
+  <div class="modal-blur"></div>
+  <div class="modal-body container">
+      <div class="works card">
+          <div class="content-box">
+              <div class="heading">
+                  <h2 class="item-title">${work.name}</h2>
+                  <ul class="close">
+                      <li class="bar-item"></li>
+                      <li class="bar-item"></li>
+                  </ul>
+              </div>
+              <div class="details">
+                  <h4>CANOPY</h4>
+                  <ul>
+                      <li>${work.stack}</li>
+                      <li>${work.year}</li>
+                  </ul>
+              </div>
+              <div class="modal-image">
+                  <img class="img" src="${work.image}" alt="Snapshoot Portfolio">
+              </div>
+              <div class="descriptions">
+                  <div class="w-70">
+                      <p>${work.description}</p>
+                  </div>
+                  <div class="tools-data w-30">
+                      <ul class="categories">
+                          ${toolsTemplate(work.tools)}
+                      </ul>
+                      <div class="clear"></div>
+                      <div class="btn-group modal-footer">
+                          <a href="${work.live_link}" class="btn">See Live <img src="assets/images/Icon.svg" alt=""></a>
+                          <a href="${work.sourse_link}" class="btn">See Source <img src="assets/images/github-in-button.svg" alt=""></a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      
+  </div>
+</div>`
+}
+
+// Tools template
+const toolsTemplate = (toolsData) => {
+  let tools = "";
+  toolsData.forEach(function (tool) {
+    tools += `
+    <li class="badge floating">${tool}</li>
+    `;
+  });
+  return tools
+}
+
+// portfolio template
 const portfolioTemplate = (portfolioData) => {
   let content = "";
   portfolioData.forEach(function (data) {
-    let tools = "";
-    data.tools.forEach(function (tool) {
-      tools += `
-      <li class="badge floating">${tool}</li>
-      `;
-    });
+    
+    
     content += `
     <div class="item card">
       <div class="item-image">
@@ -26,25 +81,26 @@ const portfolioTemplate = (portfolioData) => {
               </ul>
           </div>
           <p>${data.excerpt}</p>
-          <ul class="categories">${tools}
+          <ul class="categories">${toolsTemplate(data.tools)}
           </ul>
           <div class="clear"></div>
-          <button class="btn">See Project</button>
+          <button class="btn see-project" data-id="${data.id}">See Project</button>
       </div>
     </div>`;
   });
   return content;
 };
 
+// WINDOWS ON LOAD
 window.addEventListener("load", () => {
   const workData = [
     {
+      id: 1,
       name: "Tonic",
       image: "assets/images/Snapshoot Portfolio.png",
       stack: "Back End Dev",
       year: "2015",
-      excerpt:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+      excerpt: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
       tools: ["html", "CSS", "JavaScript"],
@@ -52,6 +108,7 @@ window.addEventListener("load", () => {
       sourse_link: "#",
     },
     {
+      id: 2,
       name: "Multi-Post Stories",
       image: "assets/images/Snapshoot Portfolio2.png",
       stack: "Full Stack Dev",
@@ -65,6 +122,7 @@ window.addEventListener("load", () => {
       sourse_link: "#",
     },
     {
+      id: 3,
       name: "FACEBOOK 360",
       image: "assets/images/Snapshoot Portfolio3.png",
       stack: "Full Stack Dev",
@@ -78,6 +136,7 @@ window.addEventListener("load", () => {
       sourse_link: "#",
     },
     {
+      id: 4,
       name: "Uber Navigation",
       image: "assets/images/Snapshoot Portfolio1.png",
       stack: "Lead Developer",
@@ -92,18 +151,52 @@ window.addEventListener("load", () => {
     },
   ];
 
+  // toggle navbar on mobile
   document.querySelector(".bars").addEventListener("click", () => {
     toggleNavbar();
   });
 
-  const menuBtn = document.querySelectorAll(".nav-link a");
-  Array.from(menuBtn).forEach((element) => {
+  const menuButton = document.querySelectorAll(".nav-link a");
+  Array.from(menuButton).forEach((element) => {
     element.addEventListener("click", () => {
       toggleNavbar();
     });
   });
 
-  // portfolio section
-  const workSectionElemnt = document.querySelector("#work-section");
-  workSectionElemnt.innerHTML = portfolioTemplate(workData);
+  // load portfolio section
+  loadPortfolio(() => {
+    const portfolioButton = document.querySelectorAll(".see-project");
+    Array.from(portfolioButton).forEach((element) => {
+      element.addEventListener("click",function(e){
+        e.preventDefault();
+        const id = this.getAttribute("data-id");
+        const filter = workData.filter(function(data){
+          return data.id == id;
+        })
+
+        // show modal
+        showModal(filter[0],closeModal)
+      })
+    })
+  });
+  
+  // function to load portfolio sections dynamically when page loads
+  function loadPortfolio (callBack = () => null) {
+    const workSectionElemnt = document.querySelector("#work-section");
+    workSectionElemnt.innerHTML = portfolioTemplate(workData);
+    return callBack();
+  }
+
+  // function to show modal when portfolio see more button is clicked
+  function showModal (modalData,callBack = () => null) {
+    document.querySelector("#modal-section").innerHTML = modal(modalData)
+    return callBack()
+  }
+
+  // function to close modal when close button is clicked
+  function closeModal () {
+    document.querySelector(".close").addEventListener("click", () => {
+      document.querySelector("#modal-section").innerHTML = ""
+    });
+  }
 });
